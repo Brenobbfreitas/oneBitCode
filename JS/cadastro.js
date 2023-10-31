0a1a024f4d1b32c3fd2b4c971ab432c68f5d9375
brenobbfreitas <brenofreitas0000@gmail.com>
2023-10-31 14:41:12 -0300

    JavaScript

---
+++ JS/cadastro.js
@@ -0,0 +1,14 @@
+alert("Bem Vindo!! A seguir pediremos que informe alguns dados.")
+
+let name = prompt("Qual o seu nome?")
+
+let age = prompt("Informe sua idade: ")
+
+let confirmAge = confirm("Sua idade é de "+ age +" anos!!") ///o confirm ele retorno o valor confirmAge True or false dependendo se foi ou não inserido
+
+alert("Seu nome: "+name)
+alert("Sua idade: "+age)
+alert("Confirmando sua idade: "+confirmAge)
+
+
+alert("Seu nome "+ name + "\n Idade: "+ age + "\n Idade Confirmada"+ confirmAge)
